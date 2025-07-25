531286e234fdb581087d37085e363e3deaf15a28
Denise Nanni <denise.nanni1@gmail.com>
2025-07-25 11:40:45 +0200

    trains can be added and removed from stations

--- src/main/scala/model/Station.scala
+++ src/main/scala/model/Station.scala
@@ -4,10 +4,23 @@ import model.Domain.StationCode
 
 trait Station:
   def code: StationCode
+  def trains: List[Train]
 
-case class SmallStation(code: StationCode) extends Station
+case class SmallStation(code: StationCode, trains: List[Train] = List.empty) extends Station
+    with TrainOperations[SmallStation]:
+  override def withTrains(newTrains: List[Train]): SmallStation = copy(trains = newTrains)
 
-case class BigStation(code: StationCode) extends Station
+case class BigStation(code: StationCode, trains: List[Train] = List.empty) extends Station
+    with TrainOperations[BigStation]:
+  override def withTrains(newTrains: List[Train]): BigStation = copy(trains = newTrains)
+
+trait TrainOperations[S <: Station]:
+  self: S =>
+  def withTrains(newTrains: List[Train]): S
+
+  def addTrain(train: Train): S = withTrains(train :: trains)
+
+  def removeTrain(train: Train): S = withTrains(trains.filterNot(_ == train))
 
 object Station:
   def bigStation(code: String): BigStation = BigStation(StationCode.fromString(code))
--- src/test/scala/model/StationTest.scala
+++ src/test/scala/model/StationTest.scala
@@ -2,6 +2,7 @@ package model
 
 import model.Domain.StationCode
 import model.Station.smallStation
+import model.Train.normalTrain
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers.*
 
@@ -10,4 +11,22 @@ class StationTest extends AnyFlatSpec:
   "A Station" should "be created with code" in {
     val station = smallStation("ST001")
     StationCode.value(station.code) should be("ST001")
+  }
+
+  it should "allow adding a train" in {
+    val station = smallStation("ST001")
+    val train = normalTrain(202)
+    val stationWithTrains = station.addTrain(train)
+    stationWithTrains.trains should contain(train)
+  }
+
+  it should "allow removing trains" in {
+    val station = smallStation("ST001")
+    val train1 = normalTrain(202)
+    val train2 = normalTrain(203)
+    val stationWithTrains = station.addTrain(train1).addTrain(train2)
+
+    val stationAfterRemoval = stationWithTrains.removeTrain(train1)
+    stationAfterRemoval.trains should not contain train1
+    stationAfterRemoval.trains should contain(train2)
   }
