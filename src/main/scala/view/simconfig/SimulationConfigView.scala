cf7468a02c5407d2c033697be1e00555b46a1eec
Denise Nanni <denise.nanni1@gmail.com>
2025-08-05 14:49:19 +0200

    train definition UI

--- src/main/scala/view/simconfig/SimulationConfigView.scala
+++ src/main/scala/view/simconfig/SimulationConfigView.scala
@@ -1,31 +1,47 @@
 package view.simconfig
 
+import scalafx.collections.ObservableBuffer
 import scalafx.geometry.Insets
-import scalafx.geometry.Pos.Center
-import scalafx.scene.control.{Button, CheckBox}
-import scalafx.scene.layout.{BorderPane, HBox, Pane, VBox}
+import scalafx.geometry.Pos.{BottomCenter, Center}
+import scalafx.scene.control.*
+import scalafx.scene.control.ScrollPane.ScrollBarPolicy.{AsNeeded, Never}
+import scalafx.scene.layout.*
 import view.{GraphUtil, View}
 
 object SimulationConfigViewConstants:
-  val DefaultWindowMinWidth = 1000
+  val DefaultWindowMinWidth = 1300
   val DefaultWindowMinHeight = 800
-  val SidebarMinWidth = 300
+  val SidebarMinWidth = 350
+  val DefaultSpacing = 10
+  val DefaultPadding: Insets = Insets(10)
+  val SmallSpacing = 5
 
 class SimulationConfigView(
     styleStrategy: StyleStrategy = DefaultStyleStrategy
 ) extends View:
 
   import SimulationConfigViewConstants.*
+  
+  // TODO: should be from controller
+  private val stations = List("Station A", "Station B", "Station C", "Station D")
 
   private val graphView = GraphView[StationView, RailView](GraphUtil.createGraph())
   private val root = createRoot
+  private lazy val sidebarContainer: Pane = new VBox():
+    prefWidth = SidebarMinWidth
+    spacing = DefaultSpacing
 
-  def initGraph(): Unit =
-    graphView.setAutomaticLayout(true)
+  private var boxes: Seq[VBox] = Seq.empty
+
+  private def applyGraphStyling(): Unit =
     graphView.edges.foreach(e => graphView.getStylableEdge(e).addStyleClass(styleStrategy.edgeStyle(e.element())))
     graphView.vertices.foreach(v =>
       graphView.getStylableVertex(v).addStyleClass(styleStrategy.vertexStyle(v.element()))
     )
+
+  def initGraph(): Unit =
+    graphView.setAutomaticLayout(true)
+    applyGraphStyling()
     graphView.init()
 
   def getRoot: Pane = root
@@ -37,11 +53,32 @@ class SimulationConfigView(
       center = graphPane
       right = sidebar
 
-  private def sidebar: Pane =
-    val button = Button("Placeholder button")
-    val vBox = new VBox(button):
-      minWidth = SidebarMinWidth
-    vBox
+  private def sidebar: VBox =
+    new VBox:
+      prefWidth = SidebarMinWidth
+      padding = DefaultPadding
+      spacing = DefaultSpacing
+      children = Seq(
+        newTrainButton,
+        trainConfigScrollPane,
+        startSimulationButton
+      )
+
+  private def newTrainButton: Button = new Button("New train"):
+    onAction = _ =>
+      boxes = boxes :+ TrainConfigGroup(stations)
+      sidebarContainer.children = boxes
+
+  private def trainConfigScrollPane: ScrollPane = new ScrollPane():
+    hbarPolicy = Never
+    vbarPolicy = AsNeeded
+    fitToWidth = true
+    style = "-fx-border-color: transparent; -fx-background-insets: 0;"
+    content = sidebarContainer
+
+  private def startSimulationButton: Button = new Button("Start simulation"):
+    alignmentInParent = BottomCenter
+    maxWidth = Double.MaxValue
 
   private def graphPane: Pane =
     val automaticLayoutCheckbox = CheckBox("Automatic layout")
@@ -55,3 +92,46 @@ class SimulationConfigView(
     new BorderPane:
       center = graphView.getView
       bottom = automaticLayoutHBox
+
+// TODO: should be stationCodes
+class TrainConfigGroup(stations: List[String]) extends VBox:
+  import SimulationConfigViewConstants.*
+
+  spacing = SmallSpacing
+  style = "-fx-stroke-width: 1; -fx-border-color: gray; -fx-padding: 5"
+
+  private val trainNameTextField =
+    new TextField:
+      promptText = "Train name"
+
+  private val highSpeedCheckBox =
+    new CheckBox("High speed")
+
+  private val stationsComboBox =
+    new ComboBox[String]:
+      promptText = "Select departure station"
+      items = ObservableBuffer(stations*)
+      
+  private val stopsCheckBoxes =
+    stations.map { station =>
+      new CheckBox(station)
+    }
+
+  private def stopsCheckboxes: ScrollPane =
+    new ScrollPane():
+      maxHeight = 300
+      content = new VBox():
+        spacing = SmallSpacing
+        padding = Insets(5)
+        children = List(new Label("Select stops")) ++ stopsCheckBoxes
+
+  private def sideBySide(node: Region*): HBox =
+    new HBox:
+      spacing = SmallSpacing
+      children = node.toSeq
+
+  children = Seq(
+    sideBySide(trainNameTextField, highSpeedCheckBox),
+    stationsComboBox,
+    stopsCheckboxes
+  )
