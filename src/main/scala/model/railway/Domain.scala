dd68d7a1f91d5a27de1870729628b70d802bc26f
Denise Nanni <denise.nanni1@gmail.com>
2025-08-06 11:36:06 +0200

    updated code creation to use apply, marked value as deprecated

--- src/main/scala/model/railway/Domain.scala
+++ src/main/scala/model/railway/Domain.scala
@@ -3,7 +3,9 @@ package model.railway
 object Domain:
   opaque type StationCode = String
   object StationCode:
+    def apply(code: String): StationCode = code
     def fromString(code: String): StationCode = code
+    @deprecated
     def value(code: StationCode): String = code
     def listOf(code1: String, codes: String*): List[StationCode] =
       (code1 :: codes.toList).map(fromString)
@@ -11,10 +13,13 @@ object Domain:
 
   opaque type RailCode = Int
   object RailCode:
+    def apply(code: Int): RailCode = code
     def fromInt(code: Int): RailCode = code
+    @deprecated
     def value(code: RailCode): Int = code
 
-  opaque type TrainCode = Int
+  opaque type TrainCode = String
   object TrainCode:
-    def fromInt(code: Int): TrainCode = code
-    def value(code: TrainCode): Int = code
+    def apply(code: String): TrainCode = code
+    def fromString(code: String): TrainCode = code
+
