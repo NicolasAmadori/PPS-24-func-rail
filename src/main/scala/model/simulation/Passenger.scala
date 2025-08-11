92453390a45512640bbf99bf1f5e9eac03b2bcea
Nicolas Amadori <nicolas.amadori@studio.unibo.it>
2025-08-12 00:06:59 +0200

    Route class created, PassengerGenerator bug fix

--- src/main/scala/model/simulation/Passenger.scala
+++ src/main/scala/model/simulation/Passenger.scala
@@ -1,16 +1,15 @@
 package model.simulation
 
 import model.railway.Domain.{StationCode, TrainCode}
-import model.railway.Rail
 import model.simulation.Domain.PassengerCode
 
 trait Passenger:
   def id: PassengerCode
   def departure: StationCode
   def destination: StationCode
-  def chosenRoute: List[Rail]
+  def route: Route
 
-case class PassengerImpl(id: PassengerCode, departure: StationCode, destination: StationCode, chosenRoute: List[Rail])
+case class PassengerImpl(id: PassengerCode, departure: StationCode, destination: StationCode, route: Route)
     extends Passenger
 
 enum PassengerState:
