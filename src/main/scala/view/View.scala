896807400ac286ece0a09896879f158a30c56f55
Nicolas Amadori <nicolas.amadori@studio.unibo.it>
2025-08-12 14:43:44 +0200

    removed unused function

--- src/main/scala/view/View.scala
+++ src/main/scala/view/View.scala
@@ -4,7 +4,3 @@ import scalafx.scene.Parent
 
 trait View:
   def getRoot: Parent
-//  def showError(error: ErrorMessage, title: String = "Error"): Unit = {
-//    // Default implementation can be overridden by specific views
-//    println(s"$title: ${error}")
-//  }
