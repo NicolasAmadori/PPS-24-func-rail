73cc8c349dee3a764ba4b416d1bdd7944c57f795
Nicolas Amadori <nicolas.amadori@studio.unibo.it>
2025-08-13 14:59:03 +0200

    changed route calculation to itinerary calculation for passengers

---
+++ src/main/scala/model/entities/Itinerary.scala
@@ -0,0 +1,63 @@
+package model.entities
+
+import model.entities.EntityCodes.StationCode
+
+/** Represents a leg of an itinerary, made with a specific train. */
+case class ItineraryLeg(train: Train, from: StationCode, to: StationCode):
+  require(train.stations.contains(from), s"Train ${train.code} does not stop at $from")
+  require(train.stations.contains(to), s"Train ${train.code} does not stop at $to")
+  require(from != to, "Departure and arrival stations must be different")
+
+  /** Returns the stations actually traveled on this leg. */
+  def stationsOnLeg: List[StationCode] =
+    val startIndex = train.stations.indexOf(from)
+    val endIndex = train.stations.indexOf(to)
+    if startIndex < endIndex then
+      train.stations.slice(startIndex, endIndex + 1)
+    else
+      train.stations.slice(endIndex, startIndex + 1).reverse
+
+  /** Total length of the leg (by summing the rails of the train's Route that are part of the leg). */
+  def length: Double =
+    val legStations = stationsOnLeg
+    train.route.rails
+      .filter(rail =>
+        legStations.contains(rail.stationA) && legStations.contains(rail.stationB)
+      )
+      .map(_.length)
+      .sum
+
+/** Represents an itinerary composed of multiple train legs. */
+case class Itinerary(legs: List[ItineraryLeg]):
+  require(legs.nonEmpty, "Itinerary cannot be empty")
+
+  /** Departure station of the itinerary. */
+  def start: StationCode = legs.head.from
+
+  /** Arrival station of the itinerary. */
+  def end: StationCode = legs.last.to
+
+  /** Visited stations in order. */
+  def stations: List[StationCode] =
+    legs.head.stationsOnLeg ++ legs.tail.flatMap(_.stationsOnLeg.drop(1))
+
+  /** Total length of the itinerary. */
+  def totalLength: Double = legs.map(_.length).sum
+
+  /** Number of train changes to make. */
+  def changeNumber: Int = legs.size - 1
+
+  override def toString: String =
+    val legsStr = legs.map { leg =>
+      s"${leg.train.code.value}: " +
+        leg.stationsOnLeg.mkString(" -> ")
+    }.mkString("\n")
+    s"""Itinerary from $start to $end:
+       |Total length: $totalLength km
+       |Legs:
+       |$legsStr
+       |""".stripMargin
+
+object Itinerary:
+  def apply(firstLeg: ItineraryLeg, otherLegs: ItineraryLeg*): Itinerary =
+    Itinerary(firstLeg +: otherLegs.toList)
--- src/main/scala/model/entities/Passenger.scala
+++ src/main/scala/model/entities/Passenger.scala
@@ -6,11 +6,21 @@ trait Passenger:
   def id: PassengerCode
   def departure: StationCode
   def destination: StationCode
-  def route: Option[Route]
+  def itinerary: Option[Itinerary]
 
-case class PassengerImpl(id: PassengerCode, departure: StationCode, destination: StationCode, route: Option[Route])
+case class PassengerImpl(id: PassengerCode, departure: StationCode, destination: StationCode, itinerary: Option[Itinerary])
     extends Passenger
 
-enum PassengerState:
+enum PassengerPosition:
   case AtStation(station: StationCode)
   case OnTrain(train: TrainCode)
+
+final case class PassengerState(
+    currentPosition: PassengerPosition,
+    previousPositions: List[PassengerPosition] = Nil
+):
+  def changePosition(newPosition: PassengerPosition): PassengerState =
+    copy(
+      currentPosition = newPosition,
+      previousPositions = previousPositions :+ currentPosition
+    )
