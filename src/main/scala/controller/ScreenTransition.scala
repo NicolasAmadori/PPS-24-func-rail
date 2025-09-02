6a239e6a85554ac4b9b68b0e4292c9f87a0bcbc4
Denise Nanni <denise.nanni1@gmail.com>
2025-09-02 10:52:02 +0200

    docs mapbuilder

--- src/main/scala/controller/ScreenTransition.scala
+++ src/main/scala/controller/ScreenTransition.scala
@@ -3,11 +3,23 @@ package controller
 import utils.StageManager
 import view.View
 
+/** Trait that enables the transition between two MVC.
+  * @tparam C
+  *   the target controller
+  * @tparam V
+  *   the target view
+  */
 trait ScreenTransition[C <: BaseController[V], V <: View]:
+  /** Creates and returns the controller and the view */
   def build(): (C, V)
 
+  /** Performs an action after the attachment of the view to the controller.
+    * @param controller
+    * @param view
+    */
   def afterAttach(controller: C, view: V): Unit = ()
 
+  /** Performs the transition */
   def transition(): Unit =
     val (controller, view) = build()
     controller.attachView(view)
